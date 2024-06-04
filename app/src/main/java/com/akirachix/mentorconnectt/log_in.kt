7f2945e4cfcd0c4bf226cfef797f2217da2ee7bf
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-06-04 11:27:12 +0300

    linked sign up page to the login page

--- app/src/main/java/com/akirachix/mentorconnectt/log_in.kt
+++ app/src/main/java/com/akirachix/mentorconnectt/log_in.kt
@@ -2,20 +2,22 @@ package com.akirachix.mentorconnectt
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class log_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_log_in)
 
-        val connect = findViewById<TextView>(R.id.textView3)
+        val connect = findViewById<TextView>(R.id.textView4)
         connect.setOnClickListener{
-            val intent = Intent(this,log_in::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
     }
