a38d5612d303fbe9e3600e6fd4fe49ca609f51d0
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-06-05 20:07:45 +0300

    Login and Sign up validation

--- app/src/main/java/com/akirachix/mentorconnectt/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnectt/MainActivity.kt
@@ -5,17 +5,90 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.akirachix.mentorconnectt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val connect = findViewById<TextView>(R.id.tvLogin)
-        connect.setOnClickListener{
-            val intent = Intent(this,MainActivityLogin::class.java)
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, MainActivityLogin::class.java)
             startActivity(intent)
+//            finish()
+
+//        val connect = findViewById<TextView>(R.id.tvLogin)
+//        connect.setOnClickListener{
+//            val intent = Intent(this,MainActivityLogin::class.java)
+//            startActivity(intent)
+//        }
         }
+        binding.btnRegister.setOnClickListener {
+            validateRegistration()
+        }
+    }
+
+    private fun validateRegistration() {
+        clearErrors()
+        var formError = false
+
+        val firstName = binding.etFirsttName.text.toString()
+        if (firstName.isBlank()) {
+            formError = true
+            binding.tilFirstName.error = getString(R.string.first_name_is_required)
+        }
+        val lastName = binding.etLastName.text.toString()
+        if (lastName.isEmpty()) {
+            formError = true
+            binding.tilLastName.error = getString(R.string.last_name_is_required)
+        }
+        val email = binding.etEmail.text.toString()
+        if (email.isEmpty()) {
+            formError = true
+            binding.tilEmail.error = getString(R.string.email_is_required)
+        }
+        val codeHive = binding.etCodehiveID.text.toString()
+        if (codeHive.isEmpty()) {
+            formError = true
+            binding.tilCodehiveID.error = getString(R.string.codehive_id_is_required)
+        }
+        val username = binding.etUserName.text.toString()
+        if (username.isEmpty()) {
+            formError = true
+            binding.tilUserName.error = getString(R.string.username_is_required)
+        }
+        val password = binding.etPassword.text.toString()
+        if (password.isEmpty()) {
+            formError = true
+            binding.tilPassword.error = getString(R.string.password_is_required)
+        }
+        val passwordConf = binding.etConfirmPassword.text.toString()
+        if (passwordConf.isEmpty()) {
+            formError = true
+            binding.tilConfirmPassword.error = getString(R.string.confirmpassword_is_required)
+        }
+        if (password != passwordConf) {
+            formError = true
+            binding.tilConfirmPassword.error =
+                getString(R.string.password_and_confirmation_do_not_match)
+        }
+        if(!formError){
+            //proceed to registration
+        }
+
+    }
+    private fun clearErrors(){
+        binding.tilFirstName.error = null
+        binding.tilLastName.error = null
+        binding.tilEmail.error = null
+        binding.tilCodehiveID.error = null
+        binding.tilUserName.error = null
+        binding.tilPassword.error = null
+        binding.tilConfirmPassword.error = null
     }
 }
--- app/src/main/java/com/akirachix/mentorconnectt/MainActivityLogin.kt
+++ app/src/main/java/com/akirachix/mentorconnectt/MainActivityLogin.kt
@@ -6,17 +6,55 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.akirachix.mentorconnectt.databinding.ActivityMainLoginBinding
 
 class MainActivityLogin : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainLoginBinding
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main_login)
-        val connect = findViewById<TextView>(R.id.tvSignup)
-        connect.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main_login)
+//        val connect = findViewById<TextView>(R.id.tvSignup)
+//        connect.setOnClickListener{
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//        }
+        binding = ActivityMainLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvSignup.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
+        binding.btnLogin.setOnClickListener {
+            validateLogin()
+        }
+
+
+    }
+
+    private fun validateLogin() {
+        clearErrors()
+        var formError = false
+
+        val username = binding.etUsername.text.toString()
+        if (username.isBlank()) {
+            formError = true
+            binding.tilUsername.error = getString(R.string.username_is_required)
+        }
+        val password = binding.etPassword.text.toString()
+        if (password.isEmpty()) {
+            formError = true
+            binding.tilPassword.error = getString(R.string.password_is_required)
+        }
+        if(!formError){
+            //proceed to registration
+        }
+    }
+    private fun clearErrors() {
+        binding.tilUsername.error = null
+        binding.tilPassword.error = null
     }
 }
