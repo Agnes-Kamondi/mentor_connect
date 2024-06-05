886701b839f1a433b79a36818146304e74531188
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-06-05 17:43:47 +0300

    Login page

--- app/src/main/java/com/akirachix/mentorconnectt/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnectt/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.akirachix.mentorconnectt
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val connect = findViewById<TextView>(R.id.textView3)
+        val connect = findViewById<TextView>(R.id.tvLogin)
         connect.setOnClickListener{
-            val intent = Intent(this,log_in::class.java)
+            val intent = Intent(this,MainActivityLogin::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/akirachix/mentorconnectt/MainActivityLogin.kt
+++ app/src/main/java/com/akirachix/mentorconnectt/MainActivityLogin.kt
@@ -1,21 +1,19 @@
 package com.akirachix.mentorconnectt
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class log_in : AppCompatActivity() {
+class MainActivityLogin : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_log_in)
-
-        val connect = findViewById<TextView>(R.id.textView4)
+        setContentView(R.layout.activity_main_login)
+        val connect = findViewById<TextView>(R.id.tvSignup)
         connect.setOnClickListener{
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
